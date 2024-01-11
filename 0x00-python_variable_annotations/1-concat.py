a3d1dd88e9d0bd0af9755be742360debff86f2d6
Dennis Adaku <dennisadaku@ymail.com>
2024-01-11 15:52:39 +0000

    added solution to task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/ python3
+"""Module containing type-annotated functions"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings
+    """
+    return str1 + str2
