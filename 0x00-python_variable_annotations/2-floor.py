2220f7fff3a3800c405929eae14d279a81ed0a04
Dennis Adaku <dennisadaku@ymail.com>
2024-01-11 15:59:13 +0000

    added solution to task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module containing type-annotated functions"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float
+    """
+    return math.floor(n)
