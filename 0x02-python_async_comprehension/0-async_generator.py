fcc92781d305bf9cd5772e5a0f8302d2fc52fa1c
Dennis Adaku <dennisadaku@ymail.com>
2024-01-16 21:12:28 +0000

    modified solution to task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yields a random integer between 1 and 10"""
     for i in range(10):
         yield random.uniform(0, 10)
